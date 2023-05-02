6960f9b07d92082279d9bbfbe12c9c386709c909
Md. Ali Azgor <aliazgoryeasin@gmail.com>
2023-05-02 17:33:43 +0600

    recipies json file creating done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const chefs = require('./data/chefs.json')
 const feedBacks = require('./data/feedback.json')
+const recipe = require('./data/cookingRecipe.json')
 
 app.use(cors()); 
 app.get('/', (req, res)=>{
@@ -14,8 +15,24 @@ app.get('/chefs', (req, res) =>{
     res.send(chefs)
 })
 
+app.get('/chefs/:id', (req, res) =>{
+    const id = req.params.id
+    const singleChef = chefs.find( c => c.id == id);
+    res.send(singleChef)
+})
+
 app.get('/feedback', (req, res) =>{
     res.send(feedBacks)
+})
+
+app.get('/recipe', (req, res) =>{
+    res.send(recipe)
+})
+
+app.get('/recipe/:id', (req, res)=>{
+    const id = req.params.id
+    const singleRecipe = recipe.find( r => r.id == id);
+    res.send(singleRecipe) 
 })
 
 app.listen(5000, ()=>{
